9cd59eb7dd5b61304b9609b8613588a175216259
Sahil Sinha <Sahil.11612944@lpu.in>
2018-03-25 12:54:48 +0530

    HomePage and WorkSpace Design Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { WordsComponent } from './words/words.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
+const appRoutes: Routes = [
+	{ path: '', component: HomeComponent},
+	{ path: 'start', component: WordsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordsComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
